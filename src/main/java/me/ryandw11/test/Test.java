33048e869dd35bc931e0e47121dcfe489004fa80
Ryandw11 <ryandw02@gmail.com>
2018-08-26 08:35:48 -0700

    test
    test

--- src/main/java/me/ryandw11/test/Test.java
+++ src/main/java/me/ryandw11/test/Test.java
@@ -5,7 +5,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Test extends JavaPlugin {
 	@Override
-	public void onEnable() {
+	public void onEnable() { //test
 		Bukkit.getLogger().warning("This is a test.");
 	}
 }
